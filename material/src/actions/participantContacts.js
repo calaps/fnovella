a9c326ea9ce7c4fd895baa94724c229d0ee2d1a2
Shahnawaz Ali <mr_shah@live.com>
2017-11-03 15:16:34 +0500

    FCN-50 done

--- material/src/actions/participantContacts.js
+++ material/src/actions/participantContacts.js
@@ -11,31 +11,70 @@ import {
   PARTICIPANT_CONTACT_DELETE_FAIL,
   PARTICIPANT_CONTACT_GET_REQUEST,
   PARTICIPANT_CONTACT_GET_SUCCESS,
-  PARTICIPANT_CONTACT_GET_FAIL
+  PARTICIPANT_CONTACT_GET_FAIL,
+  PARTICIPANT_CONTACT_GET_BY_PARTICIPANTID_REQUEST,
+  PARTICIPANT_CONTACT_GET_BY_PARTICIPANTID_SUCCESS,
+  PARTICIPANT_CONTACT_GET_BY_PARTICIPANTID_FAIL
 } from './../constants/ActionTypes';
-
-
 
 export function participantContactAddRequest(data) {
   return function (dispatch) {
-    return new Promise(function(resolve, reject){{
+    return new Promise(function (resolve, reject) {
+      {
 
+        // will be removed once API is ready
+        // dispatch({
+        //   type: PARTICIPANT_CONTACT_ADD_SUCCESS,
+        //   data
+        // });
+        // resolve(true);
+        // return;
+
+
+        // API
+        HTTP('post', '/participant_contacts/', data, {authorization: localStorage.getItem('@fnovella:token')})
+          .then(function (response) {
+            console.log("response: ", response);
+            if (!response.data.errors) {
+              dispatch({
+                type: PARTICIPANT_CONTACT_ADD_SUCCESS,
+                data: response.data.data
+              });
+              resolve(response.data);
+            } else {
+              reject(response.data);
+            }
+          })
+          .catch(error => {
+            dispatch({
+              type: PARTICIPANT_CONTACT_ADD_FAIL,
+              error: error
+            });
+            reject(error);
+          })
+      }
+    })
+  }
+}
+
+export function participantContactByParticipantIdGetRequest(id) {
+
+  return function (dispatch) {
+    return new Promise(function(resolve, reject){{
       // will be removed once API is ready
       // dispatch({
-      //   type: PARTICIPANT_CONTACT_ADD_SUCCESS,
+      //   type: PARTICIPANT_CONTACT_GET_BY_PARTICIPANTID_REQUEST,
       //   data
       // });
       // resolve(true);
       // return;
 
-
       // API
-      HTTP('post', '/participant_contacts/', data,{authorization: localStorage.getItem('@fnovella:token') })
+      HTTP('get', '/participant_contacts/'+ id +'/participant_id',null,{authorization: localStorage.getItem('@fnovella:token')})
         .then(function (response) {
-          console.log("response: ",response);
-          if(!response.data.errors){
+          if(response.data.errors === null){
             dispatch({
-              type: PARTICIPANT_CONTACT_ADD_SUCCESS,
+              type: PARTICIPANT_CONTACT_GET_BY_PARTICIPANTID_SUCCESS,
               data: response.data.data
             });
             resolve(response.data);
@@ -45,7 +84,7 @@ export function participantContactAddRequest(data) {
         })
         .catch(error => {
           dispatch({
-            type: PARTICIPANT_CONTACT_ADD_FAIL,
+            type: PARTICIPANT_CONTACT_GET_BY_PARTICIPANTID_FAIL,
             error: error
           });
           reject(error);
