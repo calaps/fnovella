65c8a5ffb2de32b23916c1ebf2dd6f886f6d96d1
Shahnawaz Ali <mr_shah@live.com>
2017-11-14 12:53:02 +0500

    minor fixes

--- material/src/routes/app/routes/course/components/EditForm.js
+++ material/src/routes/app/routes/course/components/EditForm.js
@@ -136,7 +136,7 @@ class EditForm extends React.Component {
                 .changeView('VIEW_ELEMENT');
             }
           }, (error) => {
-            alert('fail');
+            // alert('fail');
             console.log("An Error occur with the Rest API");
             self.setState({
               errors: {
@@ -158,7 +158,7 @@ class EditForm extends React.Component {
                 .changeView('VIEW_ELEMENT');
             }
           }, (error) => {
-            alert('fail');
+            // alert('fail');
             console.log("An Error occur with the Rest API");
             self.setState({
               errors: {
