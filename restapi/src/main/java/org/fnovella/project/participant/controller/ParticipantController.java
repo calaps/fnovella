238f1da3f604f0ee73e295378096d9bb0df1cdf7
elaminelaaraich <elaminelaaraich@gmail.com>
2017-11-17 23:55:03 +0000

    FCN-111: Create massive load of students service.

--- restapi/src/main/java/org/fnovella/project/participant/controller/ParticipantController.java
+++ restapi/src/main/java/org/fnovella/project/participant/controller/ParticipantController.java
@@ -1,8 +1,5 @@
 package org.fnovella.project.participant.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.fnovella.project.catalog_relation_student.repository.CatalogRelationStudentRepository;
 import org.fnovella.project.inscriptions_part_course.repository.InscriptionsPartCourseRepository;
 import org.fnovella.project.inscriptions_part_grade.repository.InscriptionsPartGradeRepository;
@@ -14,12 +11,12 @@ import org.fnovella.project.participant_contacts.repository.ParticipantContactsR
 import org.fnovella.project.utility.model.APIResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/participant/")
@@ -37,41 +34,68 @@ public class ParticipantController {
 	private InscriptionsPartGradeRepository inscriptionsPartGradeRepository;
 	@Autowired
 	private InscriptionsPartWorkshopRepository inscriptionsPartWorkshopRepository;
-	
+
 	@RequestMapping(value = "", method = RequestMethod.GET)
 	public APIResponse getAll(@RequestHeader("authorization") String authorization, Pageable pageable) {
 		return new APIResponse(this.participantRepository.findAll(pageable), null);
 	}
-	
+
 	@RequestMapping(value = "{id}", method = RequestMethod.GET)
 	public APIResponse get(@PathVariable("id") Integer id, @RequestHeader("authorization") String authorization) {
 		return new APIResponse(this.participantRepository.findOne(id), null);
 	}
-	
+
 	@RequestMapping(value = "search", method = RequestMethod.POST)
 	public APIResponse search(@RequestHeader("authorization") String authorization, Pageable pageable, ParticipantSearch participantSearch) {
 		return new APIResponse(participantSearch.getResults(this.participantRepository, pageable), null);
 	}
-	
+
 	@RequestMapping(value = "", method = RequestMethod.POST)
 	public APIResponse create(@RequestBody Participant participant, @RequestHeader("authorization") String authorization) {
 		ArrayList<String> errors = participant.validate();
 		if (errors.size() == 0) {
 			if (this.participantRepository.findByEmail(participant.getEmail()) == null) {
-				return new APIResponse(this.participantRepository.save(participant), null);	
+				return new APIResponse(this.participantRepository.save(participant), null);
 			} else {
 				errors.add("Email is already in use");
 			}
 		}
 		return new APIResponse(null, errors);
 	}
-	
+
+	@RequestMapping(value = "load", method = RequestMethod.POST)
+	public APIResponse loadMassive(@RequestBody List<Participant> participants, @RequestHeader("authorization") String authorization) {
+		final List<String> allErrors = new ArrayList<>();
+		final List<Participant> listOfParticipantsToBeSaved = new ArrayList<>();
+		int indexOfStudent[] = {1};
+		participants.stream()
+				.forEach(participant -> {
+					List<String> errors = participant.validate();
+					if (errors.size() == 0) {
+						if (this.participantRepository.findByEmail(participant.getEmail()) == null) {
+							listOfParticipantsToBeSaved.add(participant);
+						} else {
+							allErrors.add((indexOfStudent[0]++) + ":" + "Email is already in use");
+						}
+					} else {
+						List<String> currentErrors = errors.stream()
+								.map(error -> (indexOfStudent[0]++) + ":" + error)
+								.collect(Collectors.toList());
+						allErrors.addAll(currentErrors);
+					}
+				});
+		if (!CollectionUtils.isEmpty(listOfParticipantsToBeSaved)) {
+			return new APIResponse(this.participantRepository.save(listOfParticipantsToBeSaved), allErrors);
+		}
+		return new APIResponse(null, allErrors);
+	}
+
 	@RequestMapping(value = "{id}", method = RequestMethod.PATCH)
 	public APIResponse update(@PathVariable("id") Integer id, @RequestBody Participant participant, @RequestHeader("authorization") String authorization) {
 		ArrayList<String> errors = new ArrayList<String>();
 		Participant toUpdate = this.participantRepository.findOne(id);
 		if (toUpdate != null) {
-			if ((toUpdate.getEmail().equals(participant.getEmail())) || 
+			if ((toUpdate.getEmail().equals(participant.getEmail())) ||
 					(!toUpdate.getEmail().equals(participant.getEmail()) && this.participantRepository.findByEmail(participant.getEmail()) == null)) {
 				toUpdate.setUpdatedFields(participant);
 				toUpdate = this.participantRepository.saveAndFlush(participant);
@@ -79,12 +103,12 @@ public class ParticipantController {
 			} else {
 				errors.add("Email is already in use");
 			}
-		} else {			
+		} else {
 			errors.add("Participant doesn't exist");
 		}
 		return new APIResponse(null, errors);
 	}
-	
+
 	@RequestMapping(value = "{id}", method = RequestMethod.DELETE)
 	public APIResponse delete(@PathVariable("id") Integer id, @RequestHeader("authorization") String authorization) {
 		ArrayList<String> errors = new ArrayList<String>();
@@ -97,12 +121,12 @@ public class ParticipantController {
 		errors.add("Participant doesn't exist");
 		return new APIResponse(null, errors);
 	}
-	
+
 	@RequestMapping(value="delete/{id}/check", method = RequestMethod.GET)
 	public APIResponse checkDeletion(@RequestHeader("authorization") String authorization, @PathVariable("id") Integer id) {
 		return new APIResponse(this.delete(id, false), null);
 	}
-	
+
 	private boolean delete(Integer id, boolean delete) {
 		boolean toDelete = true;
 		List<?> list = this.catalogRelationStudentRepository.findByIdParticipant(id);
@@ -142,5 +166,5 @@ public class ParticipantController {
 		}
 		return toDelete;
 	}
-	
+
 }
