7c6a8780d1ecab45d7bbb2d0cfa3c97172fff6d2
Juan Pablo Ortiz <janpoloy@gmail.com>
2017-12-07 08:52:29 -0600

    more loadash liux import change

--- material/src/routes/app/routes/inscription/index.js
+++ material/src/routes/app/routes/inscription/index.js
@@ -2,8 +2,7 @@ module.exports = {
     path: 'inscription',
     getComponent(nextState, cb) {
       require.ensure([], (require) => {
-        cb(null, require('./components/inscription'));
+        cb(null, require('./components/Inscription'));
       });
     }
   };
-  
