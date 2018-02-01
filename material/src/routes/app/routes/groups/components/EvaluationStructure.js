9ec1d981c0b94a8a55d5b8e286f5b1ba971eb353
Juan Pablo Ortiz <janpoloy@gmail.com>
2018-02-01 06:29:42 -0600

    Evaluation Structura option 4 repit fixed

--- material/src/routes/app/routes/groups/components/EvaluationStructure.js
+++ material/src/routes/app/routes/groups/components/EvaluationStructure.js
@@ -313,7 +313,7 @@ class EvaluationStructure extends React.Component {
                           <option value="2">2</option>
                           <option value="3">3</option>
                           <option value="4">4</option>
-                          <option value="5">4</option>
+                          <option value="5">5</option>
                           <option value="6">6</option>
                           <option value="7">7</option>
                           <option value="8">8</option>
